b42da8147c63d4e9d1f33f5324765505f7479f64
nishant1997 <nishanttelange1997.com>
2021-02-22 07:50:23 +0530

    Botstrap-image

---
+++ src/App.js
@@ -0,0 +1,18 @@
+
+import {Image} from "react-bootstrap";
+import logo from "./Computer.jpg";
+import "./Design.css";
+function App() {
+  const my_img='./Computer.jpg';
+  return (
+    <div>
+
+      
+< Image src={logo} className={"forimg"} rounded/>
+    </div>
+
+      
+  );
+}
+
+export default App;
